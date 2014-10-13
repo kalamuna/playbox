7cb67d89501992e5ffbd5ee6334ccdb59bd9916b
Mike Pirog <mike@kalamuna.com>
2014-10-13 05:36:03 +0000

    #7: Featured battle theming

--- sites/all/modules/playbox_battles/templates/views-view-fields--playbox-battles--playbox-battles-featured-1.tpl.php
+++ sites/all/modules/playbox_battles/templates/views-view-fields--playbox-battles--playbox-battles-featured-1.tpl.php
@@ -4,100 +4,67 @@
  * Default simple view template to all the fields as a row.
  *
  *  - $view: The view in use.
- *  - $fields: an array of $field objects. Each one contains:
- *  - $field->content: The output of the field.
- *  - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
- *  - $field->class: The safe class id to use.
- *  - $field->handler: The Views field handler object controlling this field. Do not use
- *    var_export to dump this object, as it can't handle the recursion.
- *  - $field->inline: Whether or not the field should be inline.
- *  - $field->inline_html: either div or span based on the above flag.
- *  - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
- *  - $field->wrapper_suffix: The closing tag for the wrapper.
- *  - $field->separator: an optional separator that may appear before a field.
- *  - $field->label: The wrap label text to use.
- *  - $field->label_html: The full HTML of the label to use including
- *    configured element type.
+ *  - $fields: an array of $field objects.
  *  - $row: The raw result object from the query, with all data it fetched.
  *
  *  @ingroup views_templates
  *
  */
+
+// More semantic vars
+$fields == $fields;
+$battle_name = $row->node_title;
+$battle_bio = $row->field_field_playbox_battle_bio[0]['rendered'];
+
+$president_portrait = $row->field_field_playbox_portrait[0]['rendered'];
+$president_nickname = $row->field_field_playbox_nickname[0]['rendered'];
+$president_votes = $row->field_field_playbox_president_votes[0]['rendered'];
+
+$robot_portrait = $row->field_field_playbox_portrait_1[0]['rendered'];
+$robot_nickname = $row->field_field_playbox_nickname_1[0]['rendered'];
+$robot_votes = $row->field_field_playbox_robot_votes[0]['rendered'];
 ?>
 
-<?php
-
-/**
- * @file
- * Default simple view template to all the fields as a row.
- *
- * - $view: The view in use.
- * - $fields: an array of $field objects. Each one contains:
- *   - $field->content: The output of the field.
- *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
- *   - $field->class: The safe class id to use.
- *   - $field->handler: The Views field handler object controlling this field. Do not use
- *     var_export to dump this object, as it can't handle the recursion.
- *   - $field->inline: Whether or not the field should be inline.
- *   - $field->inline_html: either div or span based on the above flag.
- *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
- *   - $field->wrapper_suffix: The closing tag for the wrapper.
- *   - $field->separator: an optional separator that may appear before a field.
- *   - $field->label: The wrap label text to use.
- *   - $field->label_html: The full HTML of the label to use including
- *     configured element type.
- * - $row: The raw result object from the query, with all data it fetched.
- *
- * @ingroup views_templates
- */
-?>
-
-<?php /*
-<?php foreach ($fields as $id => $field): ?>
-  <?php if (!empty($field->separator)): ?>
-    <?php print $field->separator; ?>
-  <?php endif; ?>
-
-  <?php print $field->wrapper_prefix; ?>
-    <?php print $field->label_html; ?>
-    <?php print $field->content; ?>
-  <?php print $field->wrapper_suffix; ?>
-<?php endforeach; ?>
-*/
-
-
-?>
-
-<div class="panel-display battler clearfix <?php !empty($class) ? print $class : ''; ?>" <?php !empty($css_id) ? print "id=\"$css_id\"" : ''; ?>>
+<div class="panel-display battler-battle clearfix <?php !empty($class) ? print $class : ''; ?>" <?php !empty($css_id) ? print "id=\"$css_id\"" : ''; ?>>
   <section class="section">
     <div class="container">
       <div class="row">
-        <div class="col-md-12 battler-header-area">
-          <?php $row = $row; ?>
-          <?php $fields = $row; ?>
-          <?php $view = $view; ?>
+        <div class="col-md-12 battler-header-area text-center">
+          <div class="battler-battle-name">
+            <h2><?php print render($battle_name); ?></h2>
+          </div>
+          <div class="battler-battle-bio">
+            <?php print render($battle_bio); ?>
+          </div>
         </div>
       </div>
     </div>
   </section>
-  <section class="section">
+  <section class="section battler-vs">
     <div class="container">
       <div class="row">
-        <div class="col-md-5 battler-column-content-region-1">
+        <div class="col-md-5 battler-column-content-region-1 text-center">
+          <div <?php print drupal_attributes($options['president_attributes']); ?>>
+            <?php print render($president_portrait); ?>
+            <span class="battler-vote"><?php print render($president_votes); ?></span>
+          </div>
+          <div class="battler-nickname">
+            <?php print render($president_nickname); ?>
+          </div>
         </div>
-        <div class="col-md-2 battler-column-content-region-2">
+        <div class="col-md-2 battler-column-content-region-2 text-center">
+          <div class="battler-vs-label">VS</div>
         </div>
-        <div class="col-md-5 battler-column-content-region-3">
+        <div class="col-md-5 battler-column-content-region-3 text-center">
+          <div <?php print drupal_attributes($options['robot_attributes']); ?>>
+            <?php print render($robot_portrait); ?>
+            <span class="battler-vote"><?php print render($robot_votes); ?></span>
+          </div>
+          <div class="battler-nickname">
+            <?php print render($robot_nickname); ?>
+          </div>
         </div>
       </div>
     </div>
   </section>
-  <footer class="section" id="footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 battler-footer-area">
-        </div>
-      </div>
-    </div>
-  </footer>
 </div><!-- /.battler -->
