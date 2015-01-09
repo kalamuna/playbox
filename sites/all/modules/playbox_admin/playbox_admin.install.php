2168dea405bb613b982fd262495fee0c4de9883c
katypool <katy@kalamuna.com>
2015-01-09 15:56:20 -0800

    .install file hook_uninstall

--- sites/all/modules/playbox_admin/playbox_admin.install.php
+++ sites/all/modules/playbox_admin/playbox_admin.install.php
@@ -2,6 +2,10 @@
 /** 
  * Install file for playbox_admin
  */
-function playbox_admin_schema() {
 
+function playbox_admin_uninstall() {
+	variable_del('playbox_admin_president');
+	variable_del('playbox_admin_boss');
+	variable_del('playbox_admin_color');
+	variable_del('playbox_admin_disco');
 }
